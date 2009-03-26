70738f7c2786efcc38074fffd470e5084668299f
swhitley <swhitley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-03-26 14:07:10 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitconnect/trunk@105325 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config-sample.php
+++ config-sample.php
@@ -1,6 +1,6 @@
 <?php
 $twc_template = <<<KEEPME
-<div id="twc_connect"><p><strong>Twtter Users!</strong><br />
+<div id="twc_connect"><p><strong>Twitter Users!</strong><br />
 Enter your personal information below, or sign in with your Twitter account by clicking the button below.</p></div>
 KEEPME;
 ?>
