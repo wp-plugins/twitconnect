8b6b9751517b3744cc5680c073b06694d404f138
swhitley <swhitley@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-04-18 07:44:03 +0000

    git-svn-id: https://plugins.svn.wordpress.org/twitconnect/trunk@111453 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config-sample.php
+++ config-sample.php
@@ -1,6 +1,22 @@
 <?php
+//************************************************************************************
+//* Twitter Profile
+//* 
+//* By default, the plugin will display the url from the person's Twitter profile.
+//* This is usually their personal blog or website.
+//* If you would prefer to display the url to the person's Twitter profile instead,
+//* set $twc_use_twitter_profile = 'Y'
+//************************************************************************************
+$twc_use_twitter_profile = 'N';
+
+//************************************************************************************
+//* Template
+//* 
+//* Customize the language that appears just before the Twit Connect Button.
+//************************************************************************************
 $twc_template = <<<KEEPME
 <div id="twc_connect"><p><strong>Twitter Users!</strong><br />
-Enter your personal information below, or sign in with your Twitter account by clicking the button below.</p></div>
+Enter your personal information in the form or sign in with your Twitter account by clicking the button below.</p></div>
 KEEPME;
+
 ?>
